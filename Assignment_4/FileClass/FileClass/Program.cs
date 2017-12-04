1b95b727ddcad04148a5d72a8abef13455e0d70d
hager <hagora7779@gmail.com>
2017-12-04 11:28:37 +0200

    Assignment_4

---
+++ Assignment_4/FileClass/FileClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileClass
+{
+    interface Ifile {
+        void CreateFile();
+        void OpenFile();
+        void ClopseFile();
+        
+    }
+    
+    class file : Ifile { 
+    
+    }
+
+    class image : file { }
+    class audio : file { }
+    class video : file { }
+    class text : file { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
